5bc2b77b9d1a6950945653d7f5f42d6ecf28f26e
TaerangLee <s22051@gsm.hs.kr>
2024-05-02 12:24:17 +0900

    Add button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import * as S from "./style";
+import { useRouter } from "next/navigation";
+
+const Button = () => {
+  const { push } = useRouter();
+  return (
+    <S.ButtonWrapper>
+      <S.SpanContainer onClick={() => push(`/Naver-map`)}>
+        <S.Span1>네이버 지도 API </S.Span1>
+
+        <S.Span2>바로 가기 </S.Span2>
+      </S.SpanContainer>
+    </S.ButtonWrapper>
+  );
+};
+
+export default Button;
---
+++ src/components/Button/style.ts
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+
+export const ButtonWrapper = styled.div`
+  width: 270px;
+  height: 35px;
+  background-color: #1c9ffb;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  padding: 10px 20px;
+  align-items: center;
+`;
+
+export const SpanContainer = styled.div`
+  display: flex;
+  gap: 5px;
+  cursor: pointer;
+`;
+
+export const Span1 = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+  color: #fffa06;
+`;
+
+export const Span2 = styled.span`
+  font-size: 18px;
+  font-weight: bold;
+  color: white;
+`;
