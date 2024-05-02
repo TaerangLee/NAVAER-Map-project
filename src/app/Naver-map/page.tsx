a51aebdddaebda4d9d3c7033bdf2d2ac68a5cb8e
TaerangLee <s22051@gsm.hs.kr>
2024-05-02 16:05:43 +0900

    Add naver-map app routing, export

---
+++ src/app/Naver-map/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { NaverMaps } from "@/PageContainer";
+import Script from "next/script";
+
+const NaverMap = () => {
+  return <NaverMaps />;
+};
+
+export default NaverMap;
